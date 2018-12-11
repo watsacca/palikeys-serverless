4a6c44d91c9dcc088170959af8a6d3e43a4df483
notfb <fabian.bieker@web.de>
2018-12-11 13:07:38 +0100

    inital import

---
+++ README.md
@@ -0,0 +1,15 @@
+# Palikeys Serverless
+
+Small firebase function project to provide a REST interface for the
+[Palikeys Webapp](https://github.com/notfb/palikeys).
+
+This project is primarily offered as a gift to the Buddhist Monastic Community
+([Shangika](https://en.wikipedia.org/wiki/Sangha) [Dāna](https://en.wikipedia.org/wiki/Dāna#Buddhism)).
+You are welcome to use it under the BSD 3-clause License as you see fit.
+I kindly request you do not use this software to harm yourself or others.
+If you need this code licensed under an other open source license let me know.
+
+See LICENSE.txt for legal information.
+
+This Project was made possible by [Leanovate](https://www.leanovate.de/).
+They actually let me to do it on company time, which is very awesome!
---
+++ functions/src/index.ts
@@ -0,0 +1,50 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as firebaseHelper from 'firebase-functions-helper';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+const app = express();
+const main = express();
+const scoreCollection = 'highscore';
+
+main.use('/api', app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({extended: false}));
+
+// webApi is your functions name, and you will pass main as
+// a parameter
+export const webApi = functions.https.onRequest(main);
+
+app.post('/score', (req, res) => {
+  firebaseHelper.firestore
+    .createNewDocument(db, scoreCollection, req.body);
+  res.send('Create a new high score');
+});
+
+app.put('/score/:id', (req, res) => {
+  firebaseHelper.firestore
+    .updateDocument(db, scoreCollection, req.params.id, req.body);
+  res.send('Update a high score');
+});
+
+app.get('/score/:id', (req, res) => {
+  firebaseHelper.firestore
+    .getDocument(db, scoreCollection, req.params.id)
+    .then(doc => res.status(200).send(doc));
+});
+
+app.get('/score', (req, res) => {
+  firebaseHelper.firestore
+    .backup(db, scoreCollection)
+    .then(data => res.status(200).send(data))
+});
+
+app.delete('/score/:id', (req, res) => {
+  firebaseHelper.firestore
+    .deleteDocument(db, scoreCollection, req.params.id);
+  res.send('Document deleted');
+});
+
