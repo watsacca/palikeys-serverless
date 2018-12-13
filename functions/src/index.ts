52ad11dbaa00c499a55461b8faa20141b7d85032
notfb <fabian.bieker@web.de>
2018-12-13 08:59:45 +0100

    get by uuid

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -60,13 +60,11 @@ app.get('/score/username/:username', async (req, res) => {
 });
 
 app.get('/score/:id', async (req, res) => {
-  const snapshot = await getById(req.params.id).get();
-  if (snapshot.empty) {
-    res.status(404);
-    res.json({error: `id '${req.params.id}' not found`});
+  const score = await getByIdNonEmpty(req.params.id, res);
+  if (!score) {
     return;
   }
-  res.json(snapshot.docs[0].data());
+  res.json(score);
 });
 
 // TODO: we need to ensure on a db level that id and username are unique
@@ -91,11 +89,13 @@ app.put('/score/:id/increment', async (req, res) => {
   if (!validateScoreIncrement(obj, res)) {
     return;
   }
-  const doc = await firebaseHelper.firestore
-    .getDocument(db, scoreCollection, req.params.id);
-  doc.score += obj.score;
+  const score = await getByIdNonEmpty(req.params.id, res);
+  if (!score) {
+    return;
+  }
+  score.score += obj.score;
   firebaseHelper.firestore
-    .updateDocument(db, scoreCollection, req.params.id, doc);
+    .updateDocument(db, scoreCollection, req.params.id, score);
   res.sendStatus(204);
 });
 
@@ -107,12 +107,12 @@ app.delete('/score/:id', (req, res) => {
 
 app.get('*', (req, res) => {
   res.status(404);
-  res.json({error: 'resource not found'})
+  res.json({error: 'path not found'})
 });
 
 app.use((req, res) => {
   res.status(404);
-  res.json({error: 'not found'})
+  res.json({error: 'path not found'})
 });
 
 function validateScore(obj, res): boolean {
@@ -151,6 +151,16 @@ function getByUsername(username: string) {
 
 function getById(id: string) {
   return db.collection(scoreCollection).where('id', '==', id);
+}
+
+async function getByIdNonEmpty(id: string, res: express.Response) {
+  const snapshot = await getById(id).get();
+  if (snapshot.empty) {
+    res.status(404);
+    res.json({error: `id '${id}' not found`});
+    return undefined;
+  }
+  return snapshot.docs[0].data();
 }
 
 async function userExists(username: string): Promise<boolean> {
